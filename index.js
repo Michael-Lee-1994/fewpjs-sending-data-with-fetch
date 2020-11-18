44ceac1e92f35084bb8916734a1c31e810771b26
Michael-Lee-1994 <michael.lee.1994.flatiron@gmail.com>
2020-11-18 11:11:54 -0800

    commit tree c061a1e5607377ad6f34be6f10a98d822a92b317

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+const destinationURL = "http://localhost:3000/users";
+
+function submitData(name, email) {
+    return fetch(destinationURL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    }).then(function(response) {
+        return response.json()
+    }).then(function(object) {
+        document.body.innerHTML = object.id;
+    }).catch(function(error) {
+        document.body.innerHTML = error.message;
+    });
+}
